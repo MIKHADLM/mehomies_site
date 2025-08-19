3c71141130062673e1fd52fb1ed45ccac54ecd90
MIKHADLM <158492386+MIKHADLM@users.noreply.github.com>
2025-08-19 16:52:17 +0200

    feat: centralize Firebase config in firebase-config.js for produit.html

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+ const firebaseConfig = {
+  apiKey: "AIzaSyDwi3alZ1KoGI81Tk4EIudA6BvQb7cYlec",
+  authDomain: "mehomiesstore.firebaseapp.com",
+  projectId: "mehomiesstore",
+  storageBucket: "mehomiesstore.firebasestorage.app",
+  messagingSenderId: "109137836087",
+  appId: "1:109137836087:web:b3a0ea342f8028595e27b2",
+  measurementId: "G-JG06M8XR7F"
+};
