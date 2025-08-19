4845fce7b27dba6e9e2b84a8392cc5514320f39d
MIKHADLM <158492386+MIKHADLM@users.noreply.github.com>
2025-08-19 18:48:08 +0200

    Sécurisation: CORS allowlist, Auth/App Check, idempotence Stripe, transactions stock, CSP renforcée & règles Firestore

--- firebase-config.js
+++ firebase-config.js
@@ -7,3 +7,7 @@ export const firebaseConfig = {
   appId: "1:109137836087:web:b3a0ea342f8028595e27b2",
   measurementId: "G-JG06M8XR7F"
 };
+
+// Optional: set this to your reCAPTCHA v3 site key to enable Firebase App Check on the web client
+// Keep empty string to disable until configured in Firebase Console
+export const APP_CHECK_SITE_KEY = "";
